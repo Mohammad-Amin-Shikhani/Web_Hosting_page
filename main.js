62dbe5bb97073fe1e087418fa050d3a50fe2ba66
Mohammad-Amin-Shikhani <76825383+Mohammad-Amin-Shikhani@users.noreply.github.com>
2021-12-22 17:47:46 +0100

    This is the first upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var test = document.getElementById("test");
+var btn = document.getElementById("btn");
+var input = document.getElementById("input");
+var friends ="my name is mohammad and i come from syria ",
+    friend = friends.substring(0, )
+    test.innerHTML = friend
+
+ 
